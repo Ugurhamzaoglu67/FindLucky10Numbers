debd22ea3b2e88bdeb46d306e87f9e4f9958db11
Ugurhamzaoglu67 <craxx67@gmail.com>
2021-06-01 20:12:39 +0300

    testMetod displaying randomNumbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,56 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
+    static final int HOW_MANY_NUMBERS = 5;
+    static final int NUMBERS_OF_LOOP = 5; //1m
+
     public static void main(String[] args) {
-	// write your code here
+
+        Map<Integer,Integer> mapNumbers = new TreeMap<>();
+
+        fillInMap(mapNumbers);
+        //PRINT THIS NUMBERS
+        testMethod(mapNumbers);
+
     }
+
+    //FILL IN MAP -> WITH RAND,NO
+   private static void fillInMap(Map<Integer,Integer> mapNumbers) {
+        int loopCount = 0;
+       for(int i=0 ; i < NUMBERS_OF_LOOP; i++){
+           loopCount++;
+           int randomNumber = randomNumberGenerating();
+           if(mapNumbers.containsKey(randomNumber)){
+                    int valueCountAgain = mapNumbers.get(randomNumber);
+                    mapNumbers.put(randomNumber,++valueCountAgain);
+           }
+           else {
+               mapNumbers.put(randomNumber,1);
+           }
+       }
+
+       System.out.println("LOOP COUNT : "+loopCount);
+
+   }
+
+   // GENERATING RANDOM NUMBERS
+   private static int randomNumberGenerating() {
+        int randomNumbers = ((int)(Math.random() * HOW_MANY_NUMBERS +1 ));
+        return randomNumbers;
+
+   }
+
+    // PRINT
+   private static void testMethod(Map<Integer,Integer> mapNumbers) {
+        for( Map.Entry<Integer,Integer> numbers : mapNumbers.entrySet()){
+            System.out.println("KEY : "+numbers.getKey()+" -> VAL : "+numbers.getValue());
+        }
+   }
+
+
+
+
 }
